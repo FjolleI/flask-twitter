bbe0557652ad67c2ce6374cba37d6911918d5615
FjolleI <39909605+FjolleI@users.noreply.github.com>
2020-01-25 18:52:15 +0100

    Changed user from root to a less privileged named test

--- mysqlconnection.py
+++ mysqlconnection.py
@@ -4,7 +4,7 @@ import pymysql.cursors
 class MySQLConnection:
     def __init__(self, db):
         connection = pymysql.connect(host = 'localhost',
-                                    user = 'root', # change the user and password as needed
+                                    user = 'test', # change the user and password as needed
                                     password = '0000', 
                                     db = db,
                                     charset = 'utf8mb4',
